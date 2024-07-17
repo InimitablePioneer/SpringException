805657892a1b9d061610be26308b8d060551618a
InimitablePioneer <seungwonpioneer@gmail.com>
2024-07-17 14:56:38 +0900

    JUL17 at work via WIN

---
+++ src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,25 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterRegistration;
+import lombok.extern.java.Log;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+        filterFilterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        return filterFilterRegistrationBean;
+    }
+}
