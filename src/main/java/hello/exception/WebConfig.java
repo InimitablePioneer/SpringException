8c9d4b0ed2ceca9ea71489128e5986305b79063a
InimitablePioneer <seungwonpioneer@gmail.com>
2024-07-18 15:02:03 +0900

    JUL18 at work via WIN

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
