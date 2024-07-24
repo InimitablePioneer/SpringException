8f722c95b6f381d789fba0cea032e494142ac0c6
InimitablePioneer <seungwonpioneer@gmail.com>
2024-07-24 14:58:26 +0900

    JUL24 at work via WIN

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -12,11 +12,11 @@ public class WebServerCustomizer implements WebServerFactoryCustomizer<Configura
 
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
-//        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
-//        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
-//        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
-//
-//        factory.addErrorPages(errorPage404,errorPage500,errorPageEx);
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        factory.addErrorPages(errorPage404,errorPage500,errorPageEx);
 
     }
 }
