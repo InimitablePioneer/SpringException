dc6d14ac77b58add6d29f27a4d1980b7b53e6ba2
InimitablePioneer <145961960+InimitablePioneer@users.noreply.github.com>
2024-08-05 20:31:24 +0900

    AUG 5 at home via MAC

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,5 +1,6 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,6 +25,11 @@ public class ApiExceptionController {
             throw new UserException("사용자 오류");
         }
         return new MemberDto(id, "hello " + id);
+    }
+
+    @GetMapping("api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
     }
 
     @Data
--- src/main/java/hello/exception/exception/BadRequestException.java
+++ src/main/java/hello/exception/exception/BadRequestException.java
@@ -3,7 +3,8 @@ package hello.exception.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "잘못된 요청 오류")
+//@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "잘못된 요청 오류 rox")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "error.bad")
 public class BadRequestException extends RuntimeException {
     public static void main(String[] args) {
         System.out.println("SUMIN PSI");
